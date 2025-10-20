ea5710b0eb99ae0752751d165a0997b13cd7f475
renato <renato.j.olveira@gmail.com>
2025-10-20 00:28:50 -0300

    Trocando Chroma por FAISS

--- usando API do google/dia3_busca.py
+++ usando API do google/dia3_busca.py
@@ -1,13 +1,14 @@
 from dotenv import load_dotenv
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
 from dia2_carregar_dados import carregar_e_dividir_chunks  # ← Importa a função!
 
 load_dotenv()
 
 # 1. Obter chunks do Dia 2
-chunks = carregar_e_dividir_chunks(r"usando_API_do_google\data\dados_empresa.txt")
+chunks = carregar_e_dividir_chunks(r"usando API do google\data\dados_empresa.txt")
 # print(chunks[0].page_content)
 
 # 2. Criar embeddings e vector store
@@ -16,15 +17,15 @@ embedding_model = GoogleGenerativeAIEmbeddings(
     google_api_key=os.getenv("CHAVE_API_GOOGLE")
 )
 
-vectorstore = Chroma.from_documents(documents=chunks, embedding=embedding_model)
-retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
+vectorstore = FAISS.from_documents(documents=chunks, embedding=embedding_model)
+retriever = vectorstore.as_retriever()
 
 # 3. Testar
 pergunta = "Qual é a razão social"
 resultados = retriever.invoke(pergunta)
 
 print(f"🔍 Encontrados {len(resultados)} chunks relevantes:\n")
-print(f"🔍 Encontrados {len(resultados)} chunks relevantes:\n")
+
 
 for i, doc in enumerate(resultados):
     print(f"📄 Chunk {i+1}:")
